fe8d5ea6a08d10cfb2b9d5b62b13d8286f7a3c09
Anselmo Parente Martins <anselmoparente@gmail.com>
2023-02-10 17:18:25 -0300

    Test with cloud firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -37,8 +38,20 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  var db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
+    final user = <String, dynamic>{
+      "first": "Ada",
+      "last": "Lovelace",
+      "born": 1815
+    };
+
+// Add a new document with a generated ID
+    db.collection("users").add(user).then((DocumentReference doc) =>
+        print('DocumentSnapshot added with ID: ${doc.id}'));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
